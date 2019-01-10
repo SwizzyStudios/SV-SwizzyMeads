5755a17897b1acf178d90e97e5f641365536c809
Swizzy Studios <SwizzyStudios@gmail.com>
2019-01-09 21:08:25 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SV-SwizzyMeads
 Stardew Valley mod to add more mead flavors based on existing honey types.
+
+This mod will alter the base code for Stardew Valley to alter the Mead's name and value.  Due to the nature of the change all new mead names will be altered.  Following is the new Meads and their base value.
+
+The value was determined as honey base price * 2
+
+Wild Honey Mead (200g)
+
+Tulip Honey Mead (320g)
+
+Blue Jazz Honey Mead (400g)
+
+Summer Spangle Honey Mead (560g)
+
+Poppy Honey Mead (760g)
+
+Fairy Rose Honey Mead (1260g)
+
+Installation steps
+1. Download the mod from Nexus Mods <link>
+2. Place the SwizzyMeads folder into the Stardew Valley Mods (Steam/steamApps/common/Stardew Valley/Mods)
+3. Start up Stardew Valley through SMAPI.
+
+For more details on what Stardew Valley code was altered...
+Alters the Object.cs method "performObjectDropInAction" specifically the code around the Mead case.
---
+++ SwizzyMeads/SwizzyMeads/Object_PerformObjectDropInAction_Patcher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Harmony;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace SwizzyMeads
+{
+    [HarmonyPatch(typeof(StardewValley.Object))]
+    [HarmonyPatch("performObjectDropInAction")]
+    [HarmonyPatch(new Type[] { typeof(StardewValley.Item), typeof(bool), typeof(StardewValley.Farmer) })]
+    static class Object_PerformObjectDropInAction_Patcher
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            System.Diagnostics.Debug.WriteLine("Starting code search!");
+            var instructionsList = instructions.ToList();
+            var insertionIndex = -1;
+            var skip = 0;
+            var instructionsToInsert = new List<CodeInstruction>();
+
+            for (int i = 0; i < instructionsList.Count; i++)
+            {
+                if ((instructionsList[i].opcode == OpCodes.Ldstr) && (instructionsList[i].operand as String == "Mead"))
+                {
+                    if (skip == 1)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Found Mead!");
+                        insertionIndex = i;
+                        instructionsList[i].operand = " Mead";
+                        break;
+                    }
+                    skip++;
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("Augmenting Instructions!");
+            // ldloc.0      // object1
+            instructionsToInsert.Add(new CodeInstruction(OpCodes.Ldloc_0));
+
+            // callvirt     instance string StardewValley.Item::get_Name()
+            instructionsToInsert.Add(new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(StardewValley.Item), "get_Name")));
+
+            if (insertionIndex != - 1)
+            {
+                instructionsList.InsertRange(insertionIndex, instructionsToInsert);
+                instructionsToInsert.Clear();
+                // call         string [mscorlib]System.String::Concat(string, string)
+                instructionsToInsert.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(String), "Concat", new Type[] { typeof(String), typeof(String) })));
+                instructionsList.InsertRange(insertionIndex + 3, instructionsToInsert);
+            }
+
+            for(int i = 0; i < instructionsList.Count; i++)
+            {
+                System.Diagnostics.Debug.WriteLine("IL CODE: " + instructionsList[i].opcode + "  " + instructionsList[i].operand);
+            }
+
+            return instructionsList;
+        }
+    }
+}
